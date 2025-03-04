fad7ae25470814bdef86cd4a58ac48a09249e66a
maxencerb <dev@maxencerb.com>
2025-03-04 11:17:37 +0100

    feat: add event data

--- src/libraries/GhostBookEvents.sol
+++ src/libraries/GhostBookEvents.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+import {Tick} from "@mgv/lib/core/TickLib.sol";
+
 /// @title GhostBookEvents Library
 /// @notice Events emitted by MangroveGhostBook to track market order execution
 /// @dev Events are emitted at the start and completion of market orders
@@ -8,10 +10,12 @@ library GhostBookEvents {
   /// @notice Emitted when a market order starts execution
   /// @param taker The address of the account executing the market order
   /// @param olKeyHash Hash of the offer list key identifying the trading pair
+  /// @param module Address of the module that executed the market order
+  /// @param maxTick Maximum acceptable price
   /// @param fillVolume Volume of tokens to fill
   /// @param fillWants if true, the fillVolume is the amount of tokens the taker wants to buy
   /// @dev if false, the fillVolume is the amount of tokens the taker wants to sell
-  event OrderStarted(address indexed taker, bytes32 indexed olKeyHash, uint256 fillVolume, bool fillWants);
+  event OrderStarted(address indexed taker, bytes32 indexed olKeyHash, address module, Tick maxTick, uint256 fillVolume, bool fillWants);
 
   /// @notice Emitted when a market order completes execution
   /// @param taker The address of the account that executed the market order
