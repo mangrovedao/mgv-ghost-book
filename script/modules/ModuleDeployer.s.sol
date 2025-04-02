1157f21b016b556056c9c1cfc44b12af764e483d
Solthodox <xabierblockchain@gmail.com>
2025-04-02 12:43:23 +0200

    live test deployment + add verbosity

--- script/modules/ModuleDeployer.s.sol
+++ script/modules/ModuleDeployer.s.sol
@@ -5,23 +5,39 @@ import {MangroveGhostBookDeployer, MangroveGhostBook, console} from "../Mangrove
 
 abstract contract ModuleDeployer is MangroveGhostBookDeployer {
   address public module;
+  address public testAddress;
 
   error ERROR_DEPLOYMENT_ADDRESS_0();
 
   function deployModule() public virtual returns (address module);
 
-  function run() public override ghostBookDeployer {
+  function liveTestModule(address module) public virtual returns (bool) {
+    return true;
+  }
+
+  function run(bool testMode) public ghostBookDeployer {
     module = deployModule();
+    testAddress = vm.envOr("TEST_ADDRESS", address(0));
     if (module == address(0)) {
       revert ERROR_DEPLOYMENT_ADDRESS_0();
     }
-
-    vm.broadcast();
+    if (testMode) {
+      vm.startPrank(testAddress);
+    } else {
+      vm.startBroadcast();
+    }
     try mangroveGhostBook.whitelistModule(module) {
       console.log("Module whitelisted:", module);
     } catch (bytes memory reason) {
       console.log("Failed to whitelist module:", module);
       console.log("Current broadcaster is probably not the owner");
+    }
+    if (testMode) {
+      bool success = liveTestModule(module);
+      console.log("Live test succeeded : ", success);
+      vm.stopPrank();
+    } else {
+      vm.stopBroadcast();
     }
   }
 }
